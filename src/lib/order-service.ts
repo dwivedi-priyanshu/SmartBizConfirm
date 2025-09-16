f83a144daca04c6713943b6f93d749db1fc492c9
Priyanshu Dwivedi <priyanshudwivedi435@gmail.com>
2025-09-16 18:00:55 +0000

    i am sharing mongodb uri use that mongodb+srv://Priyanshu:1234@cluster0.

--- src/lib/order-service.ts
+++ src/lib/order-service.ts
@@ -1,35 +1,29 @@
 'use server';
 
-import fs from 'fs/promises';
-import path from 'path';
 import { Order } from './types';
+import clientPromise from './mongodb';
+import { Collection } from 'mongodb';
 
-const ordersFilePath = path.join(process.cwd(), 'src', 'lib', 'orders.json');
-
-async function readOrders(): Promise<Order[]> {
-  try {
-    const data = await fs.readFile(ordersFilePath, 'utf-8');
-    return JSON.parse(data) as Order[];
-  } catch (error) {
-    // If the file doesn't exist, return an empty array
-    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
-      return [];
-    }
-    throw error;
-  }
-}
-
-async function writeOrders(orders: Order[]): Promise<void> {
-  await fs.writeFile(ordersFilePath, JSON.stringify(orders, null, 2), 'utf-8');
+async function getOrdersCollection(): Promise<Collection<Order>> {
+    const client = await clientPromise;
+    const db = client.db();
+    return db.collection<Order>('orders');
 }
 
 export async function getOrders(): Promise<Order[]> {
-  const orders = await readOrders();
-  return orders.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  const ordersCollection = await getOrdersCollection();
+  const orders = await ordersCollection.find({}).sort({ date: -1 }).toArray();
+
+  // The _id field from MongoDB is an object and is not serializable for the client component.
+  // We need to convert it to a string.
+  return orders.map(order => ({
+      ...order,
+      // @ts-ignore
+      _id: order._id.toString(),
+  }));
 }
 
-export async function addOrder(newOrder: Order): Promise<void> {
-  const orders = await readOrders();
-  orders.push(newOrder);
-  await writeOrders(orders);
+export async function addOrder(newOrder: Omit<Order, '_id'>): Promise<void> {
+  const ordersCollection = await getOrdersCollection();
+  await ordersCollection.insertOne(newOrder as Order);
 }
